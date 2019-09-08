db09ab007b46a4042456cc5575dadbf4306c7450
Varun Horril <varunhorril@outlook.com>
2019-09-08 11:59:12 +0400

    ADD Interface for response from Guardian

---
+++ NewsPull/NewsPull.Api.Core/Interface/IGuardianResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Core.Interface
+{
+    interface IGuardianResponse
+    {
+        string Status { get; set; }
+        string UserTier { get; set; }
+        int Total { get; set; }
+        int StartIndex { get; set; }
+        int Pages { get; set; }
+        int PageSize { get; set; }
+        int CurrentPage { get; set; }
+    }
+}
