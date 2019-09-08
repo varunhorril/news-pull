6cf4a158d4321556fd1033c3d19ad26dadc91b83
Varun Horril <varunhorril@outlook.com>
2019-09-08 11:59:54 +0400

    ADD Enums for OrderBy & OrderDate

---
+++ NewsPull/NewsPull.Api.Core/Enums/OrderBy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Core.Enums
+{
+    public enum OrderBy
+    {
+        Newest,
+        Oldest,
+        Relevance
+    }
+}
---
+++ NewsPull/NewsPull.Api.Core/Enums/OrderDate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Core.Enums
+{
+    public enum OrderDate
+    {
+        Published,
+        NewspaperEdition,
+        LastModified
+    }
+}
