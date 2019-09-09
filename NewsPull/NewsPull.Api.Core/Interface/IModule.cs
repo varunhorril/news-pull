ce8885ba2b21ab8a10bb8d9b8d363488a046b769
Varun Horril <varunhorril@outlook.com>
2019-09-09 09:20:51 +0400

    ADD Interfaces IResponse & IModule

---
+++ NewsPull/NewsPull.Api.Core/Interface/IModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Core.Interface
+{
+    public interface IModule
+    {
+        IResponse Process();
+    }
+}
---
+++ NewsPull/NewsPull.Api.Core/Interface/IResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Core.Interface
+{
+    public interface IResponse
+    {
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+        object Data { get; set; }
+    }
+}
