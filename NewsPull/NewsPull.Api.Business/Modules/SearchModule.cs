ac176e92e14b1c252e0670bc57445639d6564f46
Varun Horril <varunhorril@outlook.com>
2019-09-09 11:05:07 +0400

    UPDATE SearchModule

--- NewsPull/NewsPull.Api.Business/Modules/SearchModule.cs
+++ NewsPull/NewsPull.Api.Business/Modules/SearchModule.cs
@@ -1,19 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using NewsPull.Api.Business.Configuration;
+using NewsPull.Api.Business.Helpers;
 using NewsPull.Api.Core.Interface;
+using NewsPull.Api.Core.Models;
 
 namespace NewsPull.Api.Business.Modules
 {
-    internal sealed class SearchModule : CallerBase
+    public class SearchModule : CallerBase
     {
         private string _endpoint;
+        private string _apiKey;
 
         public SearchModule()
         {
-
+            _endpoint = AppSettings.Guardian_SearchEndpoint;
+            _apiKey = ConfigHelper.GetDevApiKey();
         }
 
+        public async Task<SearchResponse> SearchByQuery(string query)
+        {
+            try
+            {
+                if (IsQueryValid(query))
+                {
+                    var request = HttpUtility.ParseQueryString(string.Empty);
+                    request["q"] = $"\"{query}\"";
+                    request["format"] = "json";
+                    request["api-key"] = _apiKey;
 
+                    return await Execute<SearchResponse>(_endpoint + $"?{request}");
+                }
+                else
+                {
+                    throw new Exception("Query is not valid.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool IsQueryValid(string query)
+        {
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
