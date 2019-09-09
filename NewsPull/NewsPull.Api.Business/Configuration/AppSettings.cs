1ba7fbc97456aec805a2081fd4059f4d773c593d
Varun Horril <varunhorril@outlook.com>
2019-09-09 09:24:23 +0400

    ADD AppSettings

---
+++ NewsPull/NewsPull.Api.Business/Configuration/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Business.Configuration
+{
+    public class AppSettings
+    {
+        public string Guardian_Endpoint { get; set; }
+        public string Guardian_SearchEndpoint { get; set; }
+    }
+}
