8f26c9f450850a257d244d7c6f43628bf9d41a62
Varun Horril <varunhorril@outlook.com>
2019-09-09 21:01:12 +0400

    ADD Models: Subscriber, AuditBase

---
+++ NewsPull/NewsPull.Api.Core/Models/AuditBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Core.Models
+{
+    public class AuditBase
+    {
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ NewsPull/NewsPull.Api.Core/Models/Subscriber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Core.Models
+{
+    public class Subscriber : AuditBase
+    {
+        public Guid SubscriberId { get; set; }
+        public string Email { get; set; }
+        public int Frequency { get; set; }
+        public string Topic { get; set; }
+    }
+}
