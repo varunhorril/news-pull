65d95fceb47df26a3b9f60d300538bea9067c545
Varun Horril <varunhorril@outlook.com>
2019-09-09 11:05:57 +0400

    ADD ApiResponse & ApiMessages

---
+++ NewsPull/NewPull.Api/Models/ApiMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewPull.Api.Models
+{
+    public static class ApiMessages
+    {
+
+        public const string OK = "OK";
+        public const string FAILED = "FAILED";
+        public const string SERVER_ERROR = "SERVER_ERROR";
+    }
+}
---
+++ NewsPull/NewPull.Api/Models/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace NewPull.Api.Models
+{
+    public class ApiResponse : IHttpActionResult
+    {
+        public string Content { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var httpResponse = new HttpResponseMessage()
+            {
+                Content = new StringContent(Content, Encoding.UTF8, "application/json"),
+                StatusCode = StatusCode
+            };
+
+            return Task.FromResult(httpResponse);
+        }
+    }
+}
