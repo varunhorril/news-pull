104f66c7a50c354a6db86ffaca31bc4972dae0ab
Varun Horril <varunhorril@outlook.com>
2019-09-09 09:23:58 +0400

    ADD Helper: ConfigHelper

---
+++ NewsPull/NewsPull.Api.Business/Helpers/ConfigHelper.cs
@@ -0,0 +1,16 @@
+﻿using NewsPull.Api.Business.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Business.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static string GetSearchEndpoint()
+        {
+            return string.Empty;
+        }
+
+    }
+}
