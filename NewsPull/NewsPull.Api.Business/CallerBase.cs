4c398a4b07bf1a7d0416f0d63ae76f4e21d766cd
Varun Horril <varunhorril@outlook.com>
2019-09-09 09:23:22 +0400

    ADD CallerBase

---
+++ NewsPull/NewsPull.Api.Business/CallerBase.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPull.Api.Business
+{
+    internal class CallerBase
+    {
+        private readonly HttpClient _client = new HttpClient();
+
+        protected async Task<T> Execute<T>(string query)
+        {
+            var response = await _client.GetAsync(query);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new InvalidOperationException("Error.");
+            }
+
+            var obj = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            return obj["response"].ToObject<T>();
+        }
+    }
+}
