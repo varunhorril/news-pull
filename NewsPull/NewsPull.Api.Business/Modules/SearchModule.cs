9b32ab0e6055aca3b738639a76c0e40c5943815b
Varun Horril <varunhorril@outlook.com>
2019-09-09 09:23:01 +0400

    ADD MODULE: SearchModule

---
+++ NewsPull/NewsPull.Api.Business/Modules/SearchModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NewsPull.Api.Core.Interface;
+
+namespace NewsPull.Api.Business.Modules
+{
+    internal sealed class SearchModule : CallerBase
+    {
+        private string _endpoint;
+
+        public SearchModule()
+        {
+
+        }
+
+
+    }
+}
