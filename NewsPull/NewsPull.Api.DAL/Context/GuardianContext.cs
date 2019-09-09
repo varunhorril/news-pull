f50582203abac735e9693fc591a05a825799e7bc
Varun Horril <varunhorril@outlook.com>
2019-09-09 21:22:06 +0400

    ADD GuardianContext & SubscriberRepository

---
+++ NewsPull/NewsPull.Api.DAL/Context/GuardianContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsPull.Api.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.DAL.Context
+{
+    public class GuardianContext : DbContext
+    {
+        public GuardianContext(DbContextOptions<GuardianContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+
+    }
+}
---
+++ NewsPull/NewsPull.Api.DAL/Repository/SubscriberRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.DAL.Repository
+{
+    public class SubscriberRepository
+    {
+
+    }
+}
