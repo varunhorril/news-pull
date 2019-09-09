07b29d6e1228002bdd3b2dc478ed70a983ad491d
Varun Horril <varunhorril@outlook.com>
2019-09-09 20:20:20 +0400

    ADD Enum: FrequencyTypes

---
+++ NewsPull/NewsPull.Api.Business/Models/FrequencyTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPull.Api.Business.Models
+{
+    public enum FrequencyTypes
+    {
+        HOURLY = 0,
+        DAILY = 1,
+        WEEKLY = 2,
+        MONTHLY = 3
+    }
+}
