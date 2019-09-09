5540748f26f418a81c7085536330714cc4d32d10
Varun Horril <varunhorril@outlook.com>
2019-09-09 20:18:42 +0400

    ADD SubsribeViewModel

---
+++ NewsPull/GuardianNews/Model/ViewModel/SubscribeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GuardianNews.Annotations;
+using GuardianNews.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardianNews.Model.ViewModel
+{
+    public class SubscribeViewModel
+    {
+        [Required(ErrorMessage = ApiMessages.MISSING_EMAIL)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = ApiMessages.MISSING_FREQUENCY)]
+        [FrequencyValidation(ErrorMessage = ApiMessages.INVALID_FREQUENCY)]
+        public string Frequency { get; set; }
+
+        [Required(ErrorMessage = ApiMessages.MISSING_TOPIC)]
+        public string Topic { get; set; }
+    }
+}
