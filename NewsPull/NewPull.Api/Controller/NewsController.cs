43a6adda45316ffa337e2eb03bc5a430fdd07739
Varun Horril <varunhorril@outlook.com>
2019-09-09 11:05:36 +0400

    ADD NewsController

---
+++ NewsPull/NewPull.Api/Controller/NewsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using NewPull.Api.Models;
+using NewsPull.Api.Business;
+using NewsPull.Api.Business.Modules;
+
+namespace NewPull.Api.Controller
+{
+    [Route("")]
+    public class NewsController : ApiController
+    {
+        [HttpGet]
+        [Route("search")]
+        public async Task<IHttpActionResult> SearchNews(string query)
+        {
+            var apiResponse = new ApiResponse()
+            {
+                Content = ApiMessages.FAILED,
+                StatusCode = HttpStatusCode.InternalServerError
+            };
+
+            try
+            {
+                var search = new SearchModule();
+                var searchResult = await search.SearchByQuery(query);
+
+            }
+            catch (Exception ex)
+            {
+                apiResponse.Content = ApiMessages.SERVER_ERROR;
+            }
+
+            return apiResponse;
+        }
+
+
+    }
+}
